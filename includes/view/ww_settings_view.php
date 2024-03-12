841c6fbfac63a0d1ac85ca5de8f9a87c6ebb395a
Cilkotron <sanjabudic2907@gmail.com>
2024-03-12 10:37:46 +0100

    Separate plugin general settings from plugin style settings

--- includes/view/ww_settings_view.php
+++ includes/view/ww_settings_view.php
@@ -13,22 +13,35 @@ $weather_text_color = get_option('ww_text_color');
 $weather_background_color = get_option('ww_background_color');
 
 // validate & save weather 
-if (isset($_POST['ww_key']) || isset($_POST['ww_maps_key']) || isset($_POST['ww_text_color']) || isset($_POST['ww_background_color'])) {
+if (isset($_POST['ww_key']) || isset($_POST['ww_maps_key'])) {
 	$api_key = sanitize_text_field($_POST['ww_key']);
 	$maps_key = sanitize_text_field($_POST['ww_maps_key']);
-	$text_color = sanitize_hex_color($_POST['ww_text_color']);
-	$background_color = sanitize_hex_color(($_POST['ww_background_color']));
 
 	$weather_key = $api_key;
 	$weather_maps_key = $maps_key;
 	update_option('ww_key', $api_key);
 	update_option('ww_maps_key', $maps_key);
-	update_option('ww_text_color', $text_color);
-	update_option('ww_background_color', $background_color);
 
 ?>
 	<div class="notice notice-success">
-		<p>Weather widget settings are updated.</p>
+		<p>Weather general widget settings are updated.</p>
+	</div>
+<?php
+}
+
+// Weather widget style settings
+if (isset($_POST['ww_text_color']) || isset($_POST['ww_background_color'])) {
+	$text_color = sanitize_hex_color($_POST['ww_text_color']);
+	$background_color = sanitize_hex_color(($_POST['ww_background_color']));
+
+	$weather_text_color = $text_color;
+	$weather_background_color = $background_color;
+
+	update_option('ww_text_color', $text_color);
+	update_option('ww_background_color', $background_color);
+?>
+	<div class="notice notice-success">
+		<p>Weather widget style settings are updated.</p>
 	</div>
 <?php
 }
@@ -37,6 +50,9 @@ if (isset($_POST['ww_key']) || isset($_POST['ww_maps_key']) || isset($_POST['ww_
 <?php if (!$weather_key) {
 	echo '<div class="notice notice-error"><p>OpenWeather API ID not found. </p></div>';
 }
+if(!$weather_maps_key) {
+	echo '<div class="notice notice-error"><p>Google maps API key not found. </p></div>';
+}
 ?>
 
 
@@ -44,23 +60,40 @@ if (isset($_POST['ww_key']) || isset($_POST['ww_maps_key']) || isset($_POST['ww_
 	<h1 class="wp-heading-inline">Weather Widget</h1>
 	<hr class="wp-header-end">
 
+	<h3 class="wp-heading-inline">General settings</h3>
+
 	<form action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>" method="post">
 
 		<table class="form-table">
 			<tr>
-				<th><label for="ww_key">OpenWeather API ID</label></th>
+				<th><label for="ww_key">OpenWeather API ID*</label></th>
 				<td>
 					<input type="text" name="ww_key" class="regular-text" id="ww_key" value="<?php echo esc_attr($weather_key ?: ''); ?>" pattern="[a-z0-9]{32}" title="Please enter a 32-character string containing only lowercase letters and numbers" required />
-					<p class="description">Please go to <a href="https://openweathermap.org/price" target="_blank">https://openweathermap.org/price</a> & subscribe for free</p>
+					<p class="description">Please go to <a href="https://openweathermap.org/price" target="_blank">https://openweathermap.org/price</a> & pick your subscription</p>
 				</td>
 			</tr>
 			<tr>
-				<th><label for="ww_maps_key">Google Maps Key</label></th>
+				<th><label for="ww_maps_key">Google Maps Key*</label></th>
 				<td>
 					<input type="text" name="ww_maps_key" class="regular-text" id="ww_maps_key" value="<?php echo esc_attr($weather_maps_key ?: ''); ?>" required />
 					<p class="description">In order to display user location on a widget you need to set up Google Maps API Key</p>
 				</td>
 			</tr>
+			<tr>
+				<th class="ww-py-0"></th>
+				<td class="ww-py-0">
+					<p class="ww-py-0 submit">
+						<input type="submit" name="ww_general_settings_submit" id="ww_general_settings_submit" class="button button-primary" value="Save WW general settings" />
+					</p>
+				</td>
+			</tr>
+		</table>
+
+	</form>
+
+	<h3 class="wp-header-inline">Style settings</h3>
+	<form action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>" method="post">
+		<table class="form-table">
 			<tr>
 				<th><label for="ww_text_color">Pick text color</label></th>
 				<td>
@@ -77,11 +110,10 @@ if (isset($_POST['ww_key']) || isset($_POST['ww_maps_key']) || isset($_POST['ww_
 				<th class="ww-py-0"></th>
 				<td class="ww-py-0">
 					<p class="ww-py-0 submit">
-						<input type="submit" name="ww_submit" id="ww_submit" class="button button-primary" value="Save Weather Widget settings" />
+						<input type="submit" name="ww_style_settings_submit" id="ww_style_settings_submit" class="button button-primary" value="Save WW style settings" />
 					</p>
 				</td>
 			</tr>
 		</table>
-
 	</form>
 </div>
